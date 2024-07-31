8c22017e3669a0b6edb9da878806a585e1418035
jewelnu <jewelice2006@gmail.com>
2024-07-31 15:21:02 +0600

    initial commit

---
+++ exam/alumni/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('members/alumni/', views.alumni_members, name='alumni_members'),
+    path('members/committee/', views.committee_members, name='committee_members'),
+    path('notice/', views.notice, name='notice'),
+    path('downloads/', views.downloads, name='downloads'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('message/', views.message, name='message'),
+    path('faq/', views.faq, name='faq'),
+]
---
+++ exam/alumni/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'alumni/home.html')
+
+def about(request):
+    return render(request, 'alumni/about.html')
+
+def alumni_members(request):
+    return render(request, 'alumni/alumni_members.html')
+
+def committee_members(request):
+    return render(request, 'alumni/committee_members.html')
+
+def notice(request):
+    return render(request, 'alumni/notice.html')
+
+def downloads(request):
+    return render(request, 'alumni/downloads.html')
+
+def gallery(request):
+    return render(request, 'alumni/gallery.html')
+
+def message(request):
+    return render(request, 'alumni/message.html')
+
+def faq(request):
+    return render(request, 'alumni/faq.html')
