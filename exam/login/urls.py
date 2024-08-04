63b30af72f0ff153df840d19dfd852ccec95ab9c
jewelnu <jewelice2006@gmail.com>
2024-08-04 22:54:43 +0600

    Forgot password integrate

--- exam/login/urls.py
+++ exam/login/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -9,6 +10,10 @@ urlpatterns = [
     path('my-login', views.my_login, name='my-log'),
     path('dashboard', views.Dashboard, name='dashboard'),
     path('logout', views.User_logout, name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='login/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='login/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='login/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='login/password_reset_complete.html'), name='password_reset_complete'),
    
 
 ]
